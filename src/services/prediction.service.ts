9e419b1810b1806e3703424428952901428b5fd6
Noth3r <azzakhrufaryannurihsan@gmail.com>
2024-06-18 23:15:56 +0700

    refactor: Update prediction service API URL to use localhost loopback address

--- src/services/prediction.service.ts
+++ src/services/prediction.service.ts
@@ -96,7 +96,7 @@ export const getPredictionApi = async (
   input: Prisma.InputDataUncheckedCreateInput & User,
   token: string
 ) => {
-  const res = await fetch("http://localhost:5000/api/predict", {
+  const res = await fetch("http://127.0.0.1:5000/api/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
